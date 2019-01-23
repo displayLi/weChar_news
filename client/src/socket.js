ef873afcc94deac198a0603e422c8d07aac2a80d
displayli <463961434@qq.com>
2019-01-23 17:45:11 +0800

    update code

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,7 @@ export default {
     ws: null,
     init(config, onMessage, onError) {
         if (!this.ws) {
-            this.ws = new WebSocket(`ws://0.0.0.0:8089/${config}`);
+            this.ws = new WebSocket(`ws://localhost:8089/${config}`);
         }
         this.ws.onmessage = event => onMessage(event.data)
 
@@ -11,6 +11,8 @@ export default {
         this.ws.onclose = _ => this.ws = null
     },
     send(text) {
-        this.ws.send(text)
+        if (text !== null) {
+            this.ws.send(text)
+        }
     }
 }
